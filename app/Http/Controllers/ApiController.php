a61fb2d1a4685a33167a74b8d91d4c67a860ed2a
DESKTOP-QJEGDBJ\hiroaki <salmon2073@gmail.com>
2017-11-06 17:49:18 +0900

    ルーティング作成

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    function post(){
+        return "here is api";
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::view('/', 'top');
+Route::view('/{val}', 'room');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/api', 'ApiController@post');
